0f9c4e091072940f73815c669c564663bb10bd8d
Bartek <barzzz12@wp.pl>
2020-04-22 13:09:55 +0200

    first commit

---
+++ src/main/java/com/example/MainController.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+	@GetMapping("/home")
+	public String hello() {
+		return "hello jenkins";
+	}
+}
